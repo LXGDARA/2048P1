7906f2a36e9ac437085d1bb3af68c44f50fdb7bd
LXGDARA <adekore8@gmail.com>
2017-01-05 12:38:54 -0800

    Better Indenting
    better indenting

--- main.cpp
+++ main.cpp
@@ -6,37 +6,35 @@ using namespace std;
     class game{
     public:
         int row2 = 1;
-
         int row1 = 0;
-
         int row3 = 2;
-
         int row4 = 3;
-
         int counter = 4;
-
-                int Size[4][4] = {};
+        int Size[4][4] = {
+            {1, 1, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
 
                 void addUp(){
                     int ind = -1;
                     int val1 = -1;
                     int val2 = 0;
-                      for(ind = -1;ind<counter;ind++){ /* counter (very important) moves elements up.*/
-
-                            for(val1 = -1; val1<counter; val1++){
-
+                    
+                    // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
+                      for(ind = -1;ind<counter;ind++){
+                          // increases position in table
+                          for(val1 = -1; val1<counter; val1++){
+                                // increases second position in which will be added
                                 for(val2 = 0; val2<counter; val2++){
 
-                                    Size[row1][ind] = Size[row1][val1] + Size[row2][val2];
-                                    Size[row2][ind] = Size[row2][val1] + Size[row3][val2];
-                                    Size[row3][ind] = Size[row3][val1] + Size[row4][val2]; //* adding the rows*//
-                                }
-
+                                    Size[row1][ind] = Size[row1][val1] + Size[row2][val2]; // places values in one box one the table in row 1
+                                    Size[row2][ind] = Size[row2][val1] + Size[row3][val2]; // places values in one box one the table in row 2
+                                    Size[row3][ind] = Size[row3][val1] + Size[row4][val2]; // places values in one box one the table in row 3
+                               }
                             }
-
                         }
-
-                  
                     system("CLS");
                    cout << Size[row1][0] << " " << Size[row1][1] << " " << Size[row1][2] << " " << Size[row1][3] << endl;
                    cout << Size[row2][0] << " " << Size[row2][1] << " " << Size[row2][2] << " " << Size[row2][3] << endl;
@@ -46,23 +44,23 @@ using namespace std;
                 }
 
                 void addRowsRt(){
-                    int ind = -1;
-                    int val1 = -1;
+                    int ind = 0;
+                    int val1 = 0;
                     int val2 = 0;
-                      for(ind = -1;ind<counter;ind++){ /* counter (very important) moves elements up.*/
-                        
-                          for(val1 = -1; val1<counter; val1++){
-                            
-                              for(val2 = 0; val2<counter; val2++){
-                                 Size[row1][ind] = Size[row1][val1] + Size[row1][val2];
-                                 Size[row2][ind] = Size[row2][val1] + Size[row2][val2];
-                                 Size[row3][ind] = Size[row3][val1] + Size[row3][val2]; //* adding the rows*//
-                                 Size[row4][ind] = Size[row4][val1] + Size[row4][val2];
+                    
+                    // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
+                    for(ind = 0;ind<counter;ind++){
+                          // increases position in table
+                          for(val1 = 0; val1<counter; val1++){
+                              // increases second position in which will be added
+                              for(val2 = 1; val2<counter; val2++){
+                                 Size[row1][ind] = Size[row1][val1] + Size[row1][val2]; // places values in one box one the table in row 1
+                                 Size[row2][ind] = Size[row2][val1] + Size[row2][val2]; // places values in one box one the table in row 2
+                                 Size[row3][ind] = Size[row3][val1] + Size[row3][val2]; // places values in one box one the table in row 3
+                                 Size[row4][ind] = Size[row4][val1] + Size[row4][val2]; // places values in one box one the table in row 4
 
-                            }
-
+                              }
                         }
-
                     }
 
                     system("CLS");
@@ -74,49 +72,48 @@ using namespace std;
                 }
                 void addRowsLf(){
                    
-                    int ind = -1;
-                    int val1 = -1;
-                    int val2 = 0;
-                        
-                        for(ind = -1;ind<counter;ind++){ /* counter (very important) moves elements up.*/
-
+                    int ind =0;
+                    int val1 = 0;
+                    int val2 = 1;
+                    
+                        // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
+                        for(ind = -1;ind<counter;ind++){
+                            // increases position in table
                             for(val1 = -1; val1<counter; val1++){
-
+                                // increases second position in which will be added
                                 for(val2 = 0; val2<counter; val2++){
 
-                                     Size[row1][ind] = Size[row1][val1] + Size[row1][val2];
-                                     Size[row2][ind] = Size[row2][val1] + Size[row2][val2];
-                                     Size[row3][ind] = Size[row3][val1] + Size[row3][val2]; //* adding the rows*//
-                                     Size[row4][ind] = Size[row4][val1] + Size[row4][val2];
-
+                                     Size[row1][ind] = Size[row1][val1] + Size[row1][val2]; // places values in one box one the table in row 1
+                                     Size[row2][ind] = Size[row2][val1] + Size[row2][val2]; // places values in one box one the table in row 2
+                                     Size[row3][ind] = Size[row3][val1] + Size[row3][val2]; // places values in one box one the table in row 3
+                                     Size[row4][ind] = Size[row4][val1] + Size[row4][val2]; // places values in one box one the table in row 4
                                 }
                             }
                         }
-
                     system("CLS");
                    cout << Size[row1][0] << " " << Size[row1][1] << " " << Size[row1][2] << " " << Size[row1][3] << endl;
                    cout << Size[row2][0] << " " << Size[row2][1] << " " << Size[row2][2] << " " << Size[row2][3] << endl;
                    cout << Size[row3][0] << " " << Size[row3][1] << " " << Size[row3][2] << " " << Size[row3][3] << endl;
                    cout << Size[row4][0] << " " << Size[row4][1] << " " << Size[row4][2] << " " << Size[row4][3] << endl;
-
                 }
                 void addRowsDn(){
 
-                    int ind = -1;
-                    int val1 = -1;
+                    int ind = 0;
+                    int val1 = 0;
                     int val2 = 0;
-                      for(ind = -1;ind<counter;ind++){ /* counter (very important) moves elements up.*/
-                     
-                        for(val1 = -1; val1<counter; val1++){
-                            
-                            for(val2 = 0; val2<counter; val2++){
+                        // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
+                        for(ind = 0;ind<counter;ind++){
+                            // increases position in table
+                            for(val1 = 0; val1<counter; val1++){
+                                // increases second position in which will be added
+                                for(val2 = 0; val2<counter; val2++){
                                 
-                                Size[row4][ind] = Size[row3][val1] +  Size[row4][val2];
-                                Size[row3][ind] = Size[row2][val1] +  Size[row3][val2];
-                                Size[row2][ind] = Size[row1][val1] +  Size[row2][val2];
+                                    Size[row4][ind] = Size[row3][val1] +  Size[row4][val2]; // places values in one box one the table in row 1
+                                    Size[row3][ind] = Size[row2][val1] +  Size[row3][val2]; // places values in one box one the table in row 2
+                                    Size[row2][ind] = Size[row1][val1] +  Size[row2][val2]; // places values in one box one the table in row 3
+                                }
                             }
-                        }
-                     }
+                         }
                     
                     system("CLS");
                    cout << Size[row1][0] << " " << Size[row1][1] << " " << Size[row1][2] << " " << Size[row1][3] << endl;
@@ -163,6 +160,7 @@ int main()
     int k = 2;
     int x;
     int y;
+        // randomizes positions to begin with or so it should
         for(k = 2; k>=0; k--){
             x = rand() % 4;
 
@@ -172,7 +170,8 @@ int main()
         }
 
     int i = 1;
-    while(i == 1){//the purpose of this code is to see if works at least
+    //the purpose of this code is to see if works at least
+    while(i == 1){
            grid.addUp();
            grid.addRowsRt();
            grid.addRowsDn();

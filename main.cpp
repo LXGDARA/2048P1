6e79cab2db59a588cbb0fd0a6de512625e32c614
LXGDARA <adekore8@gmail.com>
2017-01-13 14:41:35 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
-
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <windows.h>
 using namespace std;
     int board[4][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
-    class game{
+    struct game{
         public:
-        
         void addUp(){
             int counter = 4;
             int row2 = 1;
             int row1 = 0;
             int row3 = 2;
             int row4 = 3;
-            int ind = -1;
-            int val1 = -1;
+            int ind = 0;
+            int val1 = 0;
             int val2 = 0;
 
             // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
-            for(ind = -1;ind<counter;ind++){
+            for(ind = 0;ind<counter;ind++){
                 // increases position in table
-                for(val1 = -1; val1<counter; val1++){
+                for(val1 = 0; val1<counter; val1++){
                     // increases second position in which will be added
                     for(val2 = 0; val2<counter; val2++){
                         board[row1][ind] = board[row1][val1] + board[row2][val2]; // places values in one box one the table in row 1
@@ -68,11 +66,11 @@ using namespace std;
             int val2 = 1;
 
                 // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
-            for(ind = -1;ind<counter;ind++){
+            for(ind = 0;ind<counter;ind++){
                 // increases position in table
-                for(val1 = -1; val1<counter; val1++){
+                for(val1 = 0; val1<counter; val1++){
                     // increases second position in which will be added
-                    for(val2 = 0; val2<counter; val2++){
+                    for(val2 = 1; val2<counter; val2++){
                         board[row1][ind] = board[row1][val1] + board[row1][val2]; // places values in one box one the table in row 1
                         board[row2][ind] = board[row2][val1] + board[row2][val2]; // places values in one box one the table in row 2
                         board[row3][ind] = board[row3][val1] + board[row3][val2]; // places values in one box one the table in row 3
@@ -114,8 +112,10 @@ using namespace std;
         void boardinit(){
             int i = 0;
             while(i <2){
-                int x = rand() % 4;
-                int y = rand() % 4;
+                int x;
+                x = rand() % 4;
+                int y;
+                y = rand() % 4;
                 board[x][y] = 2;
                 i++;
             }
@@ -127,17 +127,19 @@ int main()
     game grid;
     grid.boardinit();
     // randomizes positions to begin with or so it should
-    int i = 1;
+    int i = 10;
     //the purpose of this code is to see if works at least
-    while(i == 1){
+    while(i>0){
         grid.addUp();
-        //Sleep(100 * 3);
+        Sleep(100 * 3);
         grid.addRowsRt();
-        //Sleep(100 * 3);
+        Sleep(100 * 3);
         grid.addRowsDn();
-        //Sleep(100 * 3);
+        Sleep(100 * 3);
         grid.addRowsLf();
+        Sleep(100 * 3);
         grid.ShowG();
     }
     return 0;
 }
+

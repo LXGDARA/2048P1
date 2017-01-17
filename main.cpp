e2fc03b7cc84c3f781fedf22b46a4947ab53618b
LXGDARA <adekore8@gmail.com>
2017-01-17 14:44:01 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <time.h>
 #include <windows.h>
 using namespace std;
-    int board[4][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
-    struct game{
+    int board[4][4] = {};   
+    class game{
+         
         public:
         void addUp(){
             int counter = 4;
@@ -13,23 +14,19 @@ using namespace std;
             int row1 = 0;
             int row3 = 2;
             int row4 = 3;
-            int ind = 0;
             int val1 = 0;
             int val2 = 0;
 
-            // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
-            for(ind = 0;ind<counter;ind++){
-                // increases position in table
+           // increases position in table
                 for(val1 = 0; val1<counter; val1++){
                     // increases second position in which will be added
                     for(val2 = 0; val2<counter; val2++){
-                        board[row1][ind] = board[row1][val1] + board[row2][val2]; // places values in one box one the table in row 1
-                        board[row2][ind] = board[row2][val1] + board[row3][val2]; // places values in one box one the table in row 2
-                        board[row3][ind] = board[row3][val1] + board[row4][val2]; // places values in one box one the table in row 3
+                        board[row1][val1] += board[row1][val2]; // places values in one box one the table in row 1
+                        board[row2][val1] += board[row2][val2]; // places values in one box one the table in row 2
+                        board[row3][val1] += board[row3][val2]; // places values in one box one the table in row 3}      
                     }
                 }
-            }
-        }
+           }
 
         void addRowsRt(){
             int counter = 4;
@@ -37,87 +34,96 @@ using namespace std;
             int row1 = 0;
             int row3 = 2;
             int row4 = 3;
-            int ind = 0;
             int val1 = 0;
             int val2 = 0;
 
-            // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
-            for(ind = 0;ind<counter;ind++){
                 // increases position in table
                 for(val1 = 0; val1<counter; val1++){
                     // increases second position in which will be added
                     for(val2 = 1; val2<counter; val2++){
-                        board[row1][ind] = board[row1][val1] + board[row1][val2]; // places values in one box one the table in row 1
-                        board[row2][ind] = board[row2][val1] + board[row2][val2]; // places values in one box one the table in row 2
-                        board[row3][ind] = board[row3][val1] + board[row3][val2]; // places values in one box one the table in row 3
-                        board[row4][ind] = board[row4][val1] + board[row4][val2]; // places values in one box one the table in row 4
+                        board[row1][val1] += board[row1][val2]; // places values in one box one the table in row 1
+                        board[row2][val1] += board[row2][val2]; // places values in one box one the table in row 2
+                        board[row3][val1] += board[row3][val2]; // places values in one box one the table in row 3
+                        board[row4][val1] += board[row4][val2]; // places values in one box one the table in row 4
                     }
                 }
             }
-        }
         void addRowsLf(){
             int counter = 4;
             int row2 = 1;
             int row1 = 0;
             int row3 = 2;
             int row4 = 3;
-            int ind =0;
             int val1 = 0;
             int val2 = 1;
 
-                // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
-            for(ind = 0;ind<counter;ind++){
                 // increases position in table
                 for(val1 = 0; val1<counter; val1++){
                     // increases second position in which will be added
                     for(val2 = 1; val2<counter; val2++){
-                        board[row1][ind] = board[row1][val1] + board[row1][val2]; // places values in one box one the table in row 1
-                        board[row2][ind] = board[row2][val1] + board[row2][val2]; // places values in one box one the table in row 2
-                        board[row3][ind] = board[row3][val1] + board[row3][val2]; // places values in one box one the table in row 3
-                        board[row4][ind] = board[row4][val1] + board[row4][val2]; // places values in one box one the table in row 4
+                        board[row1][val1] += board[row1][val2]; // places values in one box one the table in row 1
+                        board[row2][val1] += board[row2][val2]; // places values in one box one the table in row 2
+                        board[row3][val1] += board[row3][val2]; // places values in one box one the table in row 3
+                        board[row4][val1] += board[row4][val2]; // places values in one box one the table in row 4
                     }
                 }
-            }
-        }
+       }
         void addRowsDn(){
             int counter = 4;
             int row2 = 1;
             int row1 = 0;
             int row3 = 2;
             int row4 = 3;
-            int ind = 0;
             int val1 = 0;
             int val2 = 0;
             // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
-            for(ind = 0;ind<counter;ind++){
-                // increases position in table
                 for(val1 = 0; val1<counter; val1++){
                     // increases second position in which will be added
                     for(val2 = 0; val2<counter; val2++){
-                        board[row4][ind] = board[row3][val1] +  board[row4][val2]; // places values in one box one the table in row 1
-                        board[row3][ind] = board[row2][val1] +  board[row3][val2]; // places values in one box one the table in row 2
-                        board[row2][ind] = board[row1][val1] +  board[row2][val2]; // places values in one box one the table in row 3
+                        board[row1][val1] += board[row1][val2]; // places values in one box one the table in row 1
+                        board[row2][val1] += board[row2][val2]; // places values in one box one the table in row 2
+                        board[row3][val1] += board[row3][val2]; // places values in one box one the table in row 3
                     }
                 }
-            }
         }
-        void ShowG(){
+        void showG(){
             for(int i = 0; i <4; i++){
                 for(int j = 0; j < 4; j++){
-                    cout << board[i][j];
+                    cout << board[i][j] << "   ";
                 }
             cout << endl;
+            }
         }
-    }
-        void boardinit(){
-            int i = 0;
+        void randpos(){
+            int i;
+            int y;
+            int x;
+            showG();
+            i = 0;
             while(i <2){
-                int x;
                 x = rand() % 4;
-                int y;
                 y = rand() % 4;
-                board[x][y] = 2;
+                cout << "x:" << x << "  y:" << y << endl;
+                board[x][y] = 1;
+                showG();
                 i++;
+            }
+            cout << "finish while loop in randpos" << endl;
+        } 
+        void boardinit(){
+            int counter = 4;
+            int row2 = 1;
+            int row1 = 0;
+            int row3 = 2;
+            int row4 = 3;
+            int val1 = 0;
+            int val2 = 0; 
+            // counter is set to equal 4, and also serves as the number of times the position of the boxes will increase in a proxy place
+            for(val1 = 0;val1<counter;val1++){
+                        board[row1][val1] = 0;
+                        board[row2][val1] = 0;
+                        board[row3][val1] = 0;
+                        board[row4][val1] = 0;
             }
         }
 };
@@ -126,6 +132,7 @@ int main()
 {
     game grid;
     grid.boardinit();
+    grid.randpos();
     // randomizes positions to begin with or so it should
     int i = 10;
     //the purpose of this code is to see if works at least
@@ -138,7 +145,7 @@ int main()
         Sleep(100 * 3);
         grid.addRowsLf();
         Sleep(100 * 3);
-        grid.ShowG();
+        grid.showG();
     }
     return 0;
 }
